a09d678641175b4ab7d67dc1fcf5984bc2b4b9be
Siddhu288 <siddharthroy1511@gmail.com>
2025-04-23 10:54:48 +0530

    Add files via upload
    focus and blur

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FocusBlurDemo from './FocusBlurDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <FocusBlurDemo />
+    </div>
+  );
+}
+
+export default App;
